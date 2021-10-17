3a0e39e1cbcc37eb001c9abb7061fc7af91aaf52
Leon <73002754+icantfindmyspider@users.noreply.github.com>
2021-10-17 08:00:09 -0700

    Update script.js

--- Random-number-generator/script.js
+++ Random-number-generator/script.js
@@ -4,14 +4,9 @@
     const minimumId = document.getElementById('minimum').value;
     const maximumId = document.getElementById('maximum').value;
 
-
     let random = Math.floor(Math.random() * (maximumId - minimumId + 1));
     random += parseInt(minimumId);
         const resultId = document.getElementById('result');
         let total =resultId.innerHTML = random;
-    
-
-
-
 });
 
