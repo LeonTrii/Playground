2824679f341db4ae75c15d2f1332c73df6446ff0
Leon <73002754+icantfindmyspider@users.noreply.github.com>
2021-10-17 07:54:02 -0700

    Add files via upload

---
+++ Random-number-generator/script.js
@@ -0,0 +1,17 @@
+    const rollId = document.getElementById('roll');
+
+    rollId.addEventListener('click', () => {
+    const minimumId = document.getElementById('minimum').value;
+    const maximumId = document.getElementById('maximum').value;
+
+
+    let random = Math.floor(Math.random() * (maximumId - minimumId + 1));
+    random += parseInt(minimumId);
+        const resultId = document.getElementById('result');
+        let total =resultId.innerHTML = random;
+    
+
+
+
+});
+
