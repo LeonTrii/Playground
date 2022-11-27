bdbe39740155e54d829ffa2ae9ad957d085873ef
Leon <junzai12@icloud.com>
2022-11-26 18:56:46 -0800

    book project i can edit at school

---
+++ APLAC Book Project/src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+const Header = () => {
+  return (
+    <nav className="">
+      <ul id='nav'>
+        <li className="nav-btn">Analysis</li>
+          {
+          // Message, main idea
+          }
+        <li className="nav-btn">About the Author</li>
+        <li className="nav-btn">Purpose</li>
+        <li className="nav-btn">Audience</li>
+        <li className="nav-btn">Context</li>
+        <li className="nav-btn">Exigence</li>
+        <li className="nav-btn">Rhetorical Choices</li>
+        {
+          /** 
+           * choose from: modes, arrangements, devices, diction, syntax), appeal (at least one of the 
+            three), and tone included. 
+          */
+        }
+        <li className="nav-btn">Buy</li>
+        <li className="nav-btn">Attributions</li>
+      </ul>
+    </nav>
+  )
+}
+
+
+export default Header;
