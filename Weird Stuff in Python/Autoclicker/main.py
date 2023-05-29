ffb2714fef8a57bf6742ec51a92b22a2acb5ad37
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2023-05-29 15:04:23 -0700

    Add files via upload

---
+++ Weird Stuff in Python/Autoclicker/main.py
@@ -0,0 +1,5 @@
+import pyautogui
+pyautogui.PAUSE = 0.05
+
+while 1: 
+    pyautogui.click()
---
+++ Weird Stuff in Python/Autoclicker/part1Tester.java
@@ -0,0 +1,7 @@
+public class part1Tester 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("abrupt");
+    }    
+}
