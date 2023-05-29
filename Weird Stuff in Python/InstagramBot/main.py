9563ee54d5c892b8d6148218f2182819b59103f2
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2023-05-29 14:55:10 -0700

    Update main.py

--- Weird Stuff in Python/InstagramBot/main.py
+++ Weird Stuff in Python/InstagramBot/main.py
@@ -1,28 +1,46 @@
-from instabot import Bot;
-import os;
 from time import sleep;
-image = "./chair.jpg";
+import inspirobot;
+import requests;
+import shutil;
+from instabot import Bot;
 
-#os.rmdir
+shutil.rmtree('config');
+image1 = "./egg1.jpg";
+count = 0;
 
 '''
-
-    while 1:
-        time.sleep(1);
-        bot.comment(media_id[4], "Please do not support people who repost and don't know the creator. Give credit!");
-
+ bot.comment(media_id[4], "Pld");
 '''
 
 bot = Bot();
-bot.login(username = "pieappleii", password = open("password.txt", "r").read());
+bot.login(username = "$", password = open("password.txt", "r").read());
+
+def follow():
+    global count;
+    bot.follow_users(users);
+    count += 1;
+    print(count);
+    
+def post():
+    while 1:
+        quote = inspirobot.generate();
+        response = requests.get(quote.url, stream=True);
+
+        open(str(0) +".jpg", "wb").write(response.content)
+
+        bot.upload_photo("./" + str(0) + ".jpg", open("rickroll.txt", "r").read());
+        #os.rename("lol.jpeg.REMOVE_ME", "lol.jpeg");
+        sleep(470);
+
+    print("Response: ", bot.api.last_response);
 
 #media_id = bot.get_last_user_medias(55040474036, 5);
 #user_id = bot.get_user_id_from_username("herrscherofthunder.daily");
 
-while 1:
-    bot.upload_photo(image, caption = open("rickroll.txt", "r").read());
-    os.rename("chair.jpg.REMOVE_ME", "chair.jpg");
-    #os.rename("lol.jpeg.REMOVE_ME", "lol.jpeg");
-    sleep(25);
+def run():
+    try:
+        post();
+    except:
+        run();
 
-print("Response: ", bot.api.last_response);
+run();
