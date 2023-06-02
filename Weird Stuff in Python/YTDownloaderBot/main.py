c178bc859153f00e1eca4cff9a618ef48b8d0f74
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2023-06-02 07:31:40 -0700

    Update main.py

--- Weird Stuff in Python/YTDownloaderBot/main.py
+++ Weird Stuff in Python/YTDownloaderBot/main.py
@@ -5,6 +5,6 @@ yt = Playlist(link);
 
 # Add path to folder here
 for video in yt.videos:
-    video.streams.get_lowest_resolution().download("/Users/pieappleii/Downloads/VS Code/Python music/Music 2.0");
+    video.streams.get_lowest_resolution().download("/Users/Downloads/VS Code/Python music/Music 2.0");
     print("Video: ", video.title);
 
