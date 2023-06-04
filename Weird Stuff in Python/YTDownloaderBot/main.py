e7ae1e7ccdc763cdb94a14472d6c77cc1ba859df
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2023-06-03 21:58:39 -0700

    Update main.py

--- Weird Stuff in Python/YTDownloaderBot/main.py
+++ Weird Stuff in Python/YTDownloaderBot/main.py
@@ -1,3 +1,5 @@
+import os;
+from moviepy.editor import *
 from pytube import Playlist;
 
 link = input("YouTube playlist URL:");
@@ -5,6 +7,13 @@ yt = Playlist(link);
 
 # Add path to folder here
 for video in yt.videos:
-    video.streams.get_lowest_resolution().download("/Users/Downloads/VS Code/Python music/Music 2.0");
+    video.streams.get_lowest_resolution().download("folder_path");
     print("Video: ", video.title);
 
+for file in os.listdir("m"):
+    f = os.path.join(file);
+    path = os.path.join("folder_path",f)
+    video = VideoFileClip(path);
+    index = f.index(".mp4");
+    video.audio.write_audiofile(os.path.join("folder_path", f[0:index] + ".mp3"));
+    os.remove(path);
