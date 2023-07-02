c285068037dd3491a8175b54c7440720cd1b9e40
Leon <junzai12@icloud.com>
2023-07-02 16:18:34 -0700

    lvl29

---
+++ The-C#-Player’s-Guide-Walkthrough/Level29/WarPreparations/WarPreparations/Program.cs
@@ -0,0 +1,12 @@
+﻿Sword s1 = new Sword(Material.Iron, Gemstone.None, 10, 2);
+Sword s2 = s1 with { gemstone = Gemstone.Amber };
+Sword s3 = s1 with { gemstone = Gemstone.Amber, length = 20 };
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
+
+public record Sword(Material material, Gemstone gemstone, double length, double width);
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium };
+public enum Gemstone { None, Emerald, Amber, Sapphire, Diamond, Bitstone };
